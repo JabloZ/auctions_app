8fad9884db234dd4f451e114cd28fd6b75db51d4
JabloZ <jablozkontakt@gmail.com>
2026-03-12 22:38:58 +0100

    env added, db connection

--- src/backend/server/Start.cs
+++ src/backend/server/Start.cs
@@ -1,15 +1,30 @@
-using System;
+﻿using System;
 using System.Net;
 using System.Text;
+using Npgsql;
+namespace Start {
+    class Start {
+        public async Task RunDatabaseOperations()
+            {
+                var connString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+                await using var conn = new NpgsqlConnection(connString);
+                await conn.OpenAsync();
 
-namespace start {
-    class Program {
-        static void Main(string[] args) {
+                await using (var cmd = new NpgsqlCommand("SELECT * FROM users", conn))
+                await using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        Console.WriteLine(reader.GetInt32(0));
+                    }
+                }
+        }
+        public static async Task Main(string[] args) {
             using var listener = new HttpListener();
             listener.Prefixes.Add("http://*:8001/");
             listener.Start();
             Console.WriteLine("Serwer działa na http://localhost:8001/");
-
+            
             while (true) {
                 Console.WriteLine("react");
                 HttpListenerContext context = listener.GetContext();
@@ -34,7 +49,8 @@ namespace start {
                 using (var output = resp.OutputStream) {
                     output.Write(buffer, 0, buffer.Length);
                 }
-
+                var app=new Start();
+                await app.RunDatabaseOperations();
                 Console.WriteLine("Wysłano odpowiedź do Reacta");
             }
         }
